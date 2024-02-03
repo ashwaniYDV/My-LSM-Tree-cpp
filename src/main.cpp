cfe8219c4b8957c24caf2586d2b6b5472a135622
Ashwani Yadav <ashyadavash@gmail.com>
2024-02-03 18:24:19 +0530

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,8 @@ int main() {
     kvStore.write("k4", "{ \"name\": \"Ashwani Yadav\", \"gender\": \"Male\", \"age\": 23, \"address\": { city\": \"Kanpur\", \"state\": \"UP\" }, \"favNumber\": [4, 9, 13] }");
 
     cout << "k1 = " << kvStore.read("k1") << endl;
-    cout << "k2 = " << kvStore.read("k2") << endl;
-    cout << "k3 = " << kvStore.read("k3") << endl;
     cout << "k4 = " << kvStore.read("k4") << endl;
+    cout << "k5 = " << kvStore.read("k5") << endl;
 
     return 0;
 }
